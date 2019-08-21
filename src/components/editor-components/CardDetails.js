a9a05d042962a2fd28a1190a3a249342967b335d
fmakareev <fmakareev@code-artel.com>
2019-08-22 00:48:42 +0300

    remove unused import component. add final-form Field

--- src/components/editor-components/CardDetails.js
+++ src/components/editor-components/CardDetails.js
@@ -1,44 +1,64 @@
 import React from "react";
 import {
-    Container,
-    Row,
-    Col,
-    Card,
-    CardBody,
-    CardFooter,
-    Badge,
-    Button,
-    ButtonGroup,
-    ListGroup,
-    ListGroupItem,
-    CardHeader,
-    Form,
-    FormInput,
-    FormTextarea,
-    FormCheckbox} from "shards-react";
+  Row,
+  Card,
+  CardBody,
+  ListGroup,
+  ListGroupItem,
+  CardHeader,
+} from "shards-react";
+import {Field} from 'react-final-form'
 
-import CustomFileUpload from "../components-overview/CustomFileUpload";
+import TextareaField from "../TextareaField/TextareaField";
+import ImageField from "../ImageField/ImageField";
+import CheckboxFinalForm from "../CheckboxFinalForm/CheckboxFinalForm";
 
 const CardDetails = () => (
-    <Card small className="mb-4">
-        <CardHeader className="border-bottom">
-            <h6 className="m-0">Card details</h6>
-        </CardHeader>
-        <CardBody>
-        <ListGroup flush>
-            <ListGroupItem className="p-0 px-3">
-                <FormTextarea placeholder="Card description" className="mb-2"></FormTextarea>
-                <FormCheckbox>Article with video</FormCheckbox> 
-                <Row noGutters><h6>Thumbnail</h6></Row>
-                <Row noGutters><img width="100%" height="auto" src={require("../../images/content-management/10.jpeg")} className="pb-2"></img> </Row>
-                <Row noGutters><CustomFileUpload /></Row>
-            </ListGroupItem>
-        </ListGroup>
-        </CardBody>
-    </Card>
+  <Card small className="mb-4">
+    <CardHeader className="border-bottom">
+      <h6 className="m-0">Card details</h6>
+    </CardHeader>
+    <CardBody>
+      <ListGroup flush>
+        <ListGroupItem className="p-0 px-3">
+          <Field
+            name="card_description"
+            type="text"
+            placeholder="Card description"
+          >
+            {
+              (props) => (
+                <TextareaField className="mb-2" {...props}/>)
+            }
+          </Field>
+          <Field
+            name="with_video"
+            type="checkbox"
+          >
+            {
+              (props)=>{
+                return (<CheckboxFinalForm label={'Article with video'} {...props}/>)
+              }
+            }
+          </Field>
+
+          <Row noGutters>
+            <h6>Thumbnail</h6>
+          </Row>
+          <Field
+            name={'thumbnail'}
+          >
+            {
+              (props) => (
+                <ImageField {...props}/>)
+            }
+          </Field>
+
+        </ListGroupItem>
+      </ListGroup>
+    </CardBody>
+  </Card>
 );
-
-
 
 
 export default CardDetails;
