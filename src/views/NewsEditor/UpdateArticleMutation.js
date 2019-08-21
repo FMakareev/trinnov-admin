63a4152dd8ce4396e813a06a0b8c772da7e4da78
fmakareev <fmakareev@code-artel.com>
2019-08-22 00:47:01 +0300

    create UpdateArticleMutation

---
+++ src/views/NewsEditor/UpdateArticleMutation.js
@@ -0,0 +1,45 @@
+import {gql} from "apollo-boost";
+import {APOLLO_TYPES} from "../../apollo/Types";
+
+export const UpdateArticleMutation = gql`
+    mutation(
+        $id: String,
+        $title: String,
+        $slug: String,
+        $content: String,
+        $head_image: String,
+        $thumbnail: String,
+        $og_thumbnail: String,
+        $og_description: String,
+        $card_description: String,
+        $is_featured: String,
+        $thumbnail_featured: String,
+        $with_video: String,
+    ) {
+        createArticles(input: {
+            id: $id
+            title: $title
+            slug: $slug
+            content: $content
+            head_image: $head_image
+            thumbnail: $thumbnail
+            og_thumbnail: $og_thumbnail
+            og_description: $og_description
+            card_description: $card_description
+            is_featured: $is_featured
+            thumbnail_featured: $thumbnail_featured
+            with_video: $with_video
+        })
+        @rest(
+            type: "${APOLLO_TYPES.News}",
+            path: "/articles/",
+            method: "POST"
+            bodySerializer: "formData"
+        ) {
+            id
+            userId
+            completed
+            title
+        }
+    }
+`;
