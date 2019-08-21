4225823d754847da9e7ace8d7410f8559fb2a932
fmakareev <fmakareev@code-artel.com>
2019-08-22 00:49:27 +0300

    add allowedFileExtensions

--- src/config.js
+++ src/config.js
@@ -1,7 +1,14 @@
 
 export const Config = {
   endpoint: {
-    base: 'http://trinnov.avenirdigital.eu/api/'
+    base: '/api'
   },
-
+  allowedFileExtensions: [
+    'image/jpeg',
+    'image/x-citrix-jpeg',
+    'image/png',
+    'image/gif',
+    'image/svg+xml',
+    'image/x-icon',
+  ]
 };
