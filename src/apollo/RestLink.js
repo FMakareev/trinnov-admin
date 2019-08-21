24ae9213230b315c7b998d5f3f5e02914898e42f
fmakareev <fmakareev@code-artel.com>
2019-08-22 00:50:48 +0300

    change responseTransformer

--- src/apollo/RestLink.js
+++ src/apollo/RestLink.js
@@ -6,28 +6,45 @@ import {MultipartFormData} from "./bodySerializers/MultipartFormData";
 
 export const AppRestLink = new RestLink({
   uri: Config.endpoint.base,
-
-  responseTransformer: async response => {
+  responseTransformer: async (response, outerType) => {
     return response.json()
-      .then((response) => response.results.map((item) => ({
-        ...item,
-        __count: response.count,
-        __next: response.next,
-        __previous: response.previous,
+      .then((response) => {
+        if (Array.isArray(response.results)) {
+          return response.results.map((item) => ({
+            ...item,
+            __count: response.count,
+            __next: response.next,
+            __previous: response.previous,
 
-        __meta: {
-          count: response.count,
-          next: response.next,
-          previous: response.previous,
-        },
-      })))
+            __meta: {
+              count: response.count,
+              next: response.next,
+              previous: response.previous,
+            },
+          }))
+        }
+        if (outerType === APOLLO_TYPES.News) {
+
+          if (response.content) {
+            try {
+              response.content = JSON.parse(response.content);
+              return response;
+            } catch (e) {
+              return response;
+            }
+          }
+
+          return response;
+        }
+        return response;
+      })
   },
 
   bodySerializers: {
     formData: MultipartFormData
   },
   typePatcher: {
-    [APOLLO_TYPES.News]: (data, outerType, ) => ({
+    [APOLLO_TYPES.News]: (data, outerType,) => ({
       ...data,
       __typename: outerType
     }),
