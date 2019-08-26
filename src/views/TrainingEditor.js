d3b6cfc28b70e16aa55c29338699a6d123864383
fmakareev <fmakareev@code-artel.com>
2019-08-26 12:23:12 +0300

    Remove unused imports. Add final-form.

--- src/views/TrainingEditor.js
+++ src/views/TrainingEditor.js
@@ -5,77 +5,63 @@ import {
   Container,
   Row,
   Col,
-  Card,
-  CardBody,
-  CardFooter,
-  Badge,
   Button,
-  ButtonGroup,
-  ListGroup,
-  ListGroupItem,
-  CardHeader,
-  Form,
-  FormInput,
-  FormTextarea,
-  FormCheckbox
 } from "shards-react";
 
 import PageTitle from "../components/common/PageTitle";
-import Helmet from "react-helmet"; 
-import CustomFileUpload from "../components/components-overview/CustomFileUpload";
-import ReactQuill from "react-quill";
-import {$, window} from "jquery";
 import OpenGraph from "../components/OpenGraph";
 import CardDetails from "../components/editor-components/CardDetails";
-import CardFeatured from "../components/editor-components/CardFeatured";
 import SidebarCategories from "../components/add-new-post/SidebarCategories";
-import ArticleHeadImage from "../components/editor-components/ArticleHeadImage";
 
-import TrainingArticleEditor from "../components/editor-components/TrainingArticleEditor";
+import TrainingArticleEditor
+  from "../components/editor-components/TrainingArticleEditor";
+import {Form} from "react-final-form";
 
 
+const TrainingEditor = () => {
 
-class TrainingEditor extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-        
-      };
-  }
+  return (<Form
+    onSubmit={(values) => {
+      console.log(values);
+    }}
+    render={({
+               handleSubmit,
+               values
+             }) => {
+      return (
+        <form onSubmit={handleSubmit}>
+          <Container fluid className="main-content-container px-4">
+            {/* Page Header */}
+            <Row noGutters className="page-header py-4">
+              <PageTitle sm="4" title="Trainings editor"
+                         subtitle="Drag and drop interface"
+                         className="text-sm-left"/>
+              <div className="ml-auto">
+                <Button outline theme="accent" size="sm" className="mr-2">
+                  <i className="material-icons">save</i> Save Draft
+                </Button>
+                <Button theme="accent" size="sm" className="ml-auto">
+                  <i className="material-icons">file_copy</i> Publish
+                </Button>
+              </div>
+            </Row>
+            <Row className="mb-4">
+              <Col lg="9">
+                <TrainingArticleEditor/>
+              </Col>
+              <Col lg="3">
+                <OpenGraph/>
+                <CardDetails/>
+                <SidebarCategories/>
+              </Col>
+            </Row>
+          </Container>
+        </form>
+      );
+    }}/>)
 
-  render() {
-    const {
-       
-    } = this.state;
 
-    return (
-    <Container fluid className="main-content-container px-4"> 
-      {/* Page Header */}
-      <Row noGutters className="page-header py-4">
-          <PageTitle sm="4" title="Trainings editor" subtitle="Drag and drop interface" className="text-sm-left" />
-          <div className="ml-auto">
-          <Button outline theme="accent" size="sm" className="mr-2">
-            <i className="material-icons">save</i> Save Draft
-          </Button>
-          <Button theme="accent" size="sm" className="ml-auto">
-            <i className="material-icons">file_copy</i> Publish
-          </Button>
-          </div>
-      </Row>
-      <Row className="mb-4">
-        <Col lg="9">
-          <TrainingArticleEditor />
-        </Col>
-        <Col lg="3">
-          <OpenGraph />
-          <CardDetails />
-          <SidebarCategories />
-        </Col>
-      </Row>
-    </Container>
-    );
-  }
 }
 
 export default TrainingEditor;
