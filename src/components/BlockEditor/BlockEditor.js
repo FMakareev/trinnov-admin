eb8a5138cf3f70a37ed44c1cfc7cef72218b8c32
fmakareev <fmakareev@code-artel.com>
2019-09-01 11:56:34 +0300

    add editorjs tools: Paragraph, MultiquoteTools, ImageTool, EmbedVideoTool, ParagraphWithImage. Add style

--- src/components/BlockEditor/BlockEditor.js
+++ src/components/BlockEditor/BlockEditor.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactEditorJs from 'react-editor-js';
 import classNames from "classnames";
+import './BlockEditorStyle.css';
+import Paragraph from "./plugins/paragraph";
+import {MultiquoteTools} from "./plugins/MultiquoteTools/Multiquote";
+import ImageTool from "@editorjs/image";
+import EmbedVideoTool from "@editorjs/embed";
+import {ParagraphWithImage} from "./plugins/paragraphWithImage";
 
 
 const initData = (value) => {
@@ -22,7 +28,52 @@ const initData = (value) => {
   }
 };
 
+const tools = {
+  paragraphWithImage: {
+    class: ParagraphWithImage,
+    config: {
+      imageCaption: false,
+      endpoints: {
+        byFile: '/editor/transport',
+        byUrl: '/editor/transport',
+      }
+    }
+  },
+  multiquote: MultiquoteTools,
+  intro: {
+    class: Paragraph,
+    config: {
+      css: {
+        wrapper: ['intro_text']
+      },
+    },
+    inlineToolbar: true,
+  },
+  embed: {
+    class: EmbedVideoTool,
+    inlineToolbar: true,
+
+    config: {
+      services: {
+        youtube: true,
+        coub: true,
+      }
+    }
+  },
+  image: {
+    class: ImageTool,
+    config: {
+      endpoints: {
+        byFile: '/editor/transport',
+        byUrl: '/editor/transport',
+      }
+    }
+  }
+};
+
+
 const BlockEditor = ({input, meta: {touched, error, submitError}, EditorRefInstance}) => {
+
   return (
     <div
       className={"form-control"}
@@ -30,6 +81,13 @@ const BlockEditor = ({input, meta: {touched, error, submitError}, EditorRefInsta
         position: 'relative',
         padding: '.4375rem .75rem .4375rem 1.5rem'
       }}>
+
+      {/*<button type={'button'} onClick={async () => {*/}
+      {/*  const data = await EditorRefInstance.EditorRefInstance.save();*/}
+      {/*  console.log(JSON.stringify(data));*/}
+      {/*}}>*/}
+      {/*  save*/}
+      {/*</button>*/}
       {/* this is for auto focus submitError */}
       <input
         {...input}
@@ -54,6 +112,8 @@ const BlockEditor = ({input, meta: {touched, error, submitError}, EditorRefInsta
       }
       <ReactEditorJs
         data={initData(input.value)}
+
+        tools={tools}
         instanceRef={(instance) => {
           EditorRefInstance
           && EditorRefInstance.addEditorRefInstance
