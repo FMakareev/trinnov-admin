4eda4568ef1c7b68905be2c9d0b03e5bf1cdc0dd
fmakareev <fmakareev@code-artel.com>
2019-09-01 11:59:10 +0300

    Create paragraph icon

---
+++ src/components/BlockEditor/plugins/paragraph/IconParagraph.js
@@ -0,0 +1,10 @@
+
+export const IconParagraph = `
+<svg xmlns="http://www.w3.org/2000/svg" 
+version="1.1" 
+width="16" 
+height="16" 
+viewBox="0 0 16 16">
+<path fill="inherit" d="M5.5 0c-2.5 0-4.5 2-4.5 4.5s2 4.5 4.5 4.5h2.5v7h2v-14h1v14h2v-14h2v-2h-9.5z"/>
+</svg>
+`
