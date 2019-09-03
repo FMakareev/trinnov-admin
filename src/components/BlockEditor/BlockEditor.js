b4f64104949ca66d8d793eb734ae861cdd6a6104
fmakareev <fmakareev@code-artel.com>
2019-09-03 11:43:15 +0300

    add plugins TabsEmbed, GridTextBlock,TextWithTitle, TitleWithLogoWithText, TitleWithTextWithLogo, TitleWithTextWithGallery

--- src/components/BlockEditor/BlockEditor.js
+++ src/components/BlockEditor/BlockEditor.js
@@ -4,9 +4,17 @@ import classNames from "classnames";
 import './BlockEditorStyle.css';
 import Paragraph from "./plugins/paragraph";
 import {MultiquoteTools} from "./plugins/MultiquoteTools/Multiquote";
-import ImageTool from "@editorjs/image";
-import EmbedVideoTool from "@editorjs/embed";
+import ImageTool from "./plugins/imageTool";
+import EmbedVideoTool from "./plugins/embed";
 import {ParagraphWithImage} from "./plugins/paragraphWithImage";
+import {TabsEmbed} from "./plugins/TabsEmbed";
+import GridTextBlock from "./plugins/gridTextBlock";
+import {TextWithTitle} from "./plugins/textWithTitle";
+import {TitleWithLogoWithText} from "./plugins/titleWithLogoWithText";
+import {TitleWithTextWithLogo} from "./plugins/titleWithTextWithLogo";
+import {TitleWithList} from "./plugins/titleWithList";
+import {List} from "./plugins/list";
+import {TitleWithTextWithGallery} from "./plugins/titleWithTextWithGallery";
 
 
 const initData = (value) => {
@@ -29,6 +37,27 @@ const initData = (value) => {
 };
 
 const tools = {
+  titleWithText: TextWithTitle,
+  titleWithLogoWithText: TitleWithLogoWithText,
+  titleWithTextWithLogo: TitleWithTextWithLogo,
+  // titleWithList: TitleWithList,
+  TitleWithTextWithGallery: TitleWithTextWithGallery,
+  gridTextBlock: {
+    class: GridTextBlock,
+    config: {
+      col: 2,
+    }
+  },
+  tabsEmbed: {
+    class: TabsEmbed,
+    config: {
+      col: 2,
+      services: {
+        youtube: true,
+        coub: true,
+      },
+    }
+  },
   paragraphWithImage: {
     class: ParagraphWithImage,
     config: {
@@ -43,6 +72,7 @@ const tools = {
   intro: {
     class: Paragraph,
     config: {
+      placeholder: 'Enter text',
       css: {
         wrapper: ['intro_text']
       },
@@ -64,8 +94,10 @@ const tools = {
     class: ImageTool,
     config: {
       endpoints: {
-        byFile: '/editor/transport',
-        byUrl: '/editor/transport',
+        // byFile: '/editor/transport',
+        // byUrl: '/editor/transport',
+        byFile: '/api/gallery/',
+        byUrl: '/api/gallery/',
       }
     }
   }
@@ -82,12 +114,12 @@ const BlockEditor = ({input, meta: {touched, error, submitError}, EditorRefInsta
         padding: '.4375rem .75rem .4375rem 1.5rem'
       }}>
 
-      {/*<button type={'button'} onClick={async () => {*/}
-      {/*  const data = await EditorRefInstance.EditorRefInstance.save();*/}
-      {/*  console.log(JSON.stringify(data));*/}
-      {/*}}>*/}
-      {/*  save*/}
-      {/*</button>*/}
+      <button type={'button'} onClick={async () => {
+        const data = await EditorRefInstance.EditorRefInstance.save();
+        console.log(JSON.stringify(data));
+      }}>
+        save
+      </button>
       {/* this is for auto focus submitError */}
       <input
         {...input}
@@ -112,7 +144,6 @@ const BlockEditor = ({input, meta: {touched, error, submitError}, EditorRefInsta
       }
       <ReactEditorJs
         data={initData(input.value)}
-
         tools={tools}
         instanceRef={(instance) => {
           EditorRefInstance
