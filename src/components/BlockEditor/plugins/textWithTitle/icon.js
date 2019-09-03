6fff0d3d19cf6fa7e1f085af7e92d62aefe577fa
fmakareev <fmakareev@code-artel.com>
2019-09-03 11:40:10 +0300

    Create text with title plugin

---
+++ src/components/BlockEditor/plugins/textWithTitle/icon.js
@@ -0,0 +1,11 @@
+
+export const IconTitleWithText = `<svg width="20" height="26" viewBox="0 0 30 38" fill="transparent" xmlns="http://www.w3.org/2000/svg">
+<rect x="1" y="1" width="28" height="36" rx="1" fill="white" stroke="currentColor" stroke-width="2"/>
+<rect x="4" y="4" width="22" height="6" fill="currentColor"/>
+<rect x="4" y="14" width="20" height="2" fill="currentColor"/>
+<rect x="4" y="18" width="16" height="2" fill="currentColor"/>
+<rect x="4" y="22" width="18" height="2" fill="currentColor"/>
+<rect x="4" y="26" width="15" height="2" fill="currentColor"/>
+<rect x="4" y="30" width="19" height="2" fill="currentColor"/>
+</svg>
+`
---
+++ src/components/BlockEditor/plugins/textWithTitle/index.js
@@ -0,0 +1,95 @@
+import {CreateDOMElement} from "../../utils/createDOMElement";
+import Header from "@editorjs/header";
+import Paragraph from "../paragraph";
+import IconGrid from "../gridTextBlock/iconGrid";
+import {IconTitleWithText} from "./icon";
+
+
+export class TextWithTitle {
+
+  _data = {};
+
+  view = {
+    wrapper: null,
+    header: null,
+    text: null,
+  };
+
+  api = null;
+  config = null;
+
+  static get toolbox() {
+    return {
+      title: 'Title and text',
+      icon: IconTitleWithText,
+    };
+  }
+
+
+  constructor({api,data,config}) {
+    this.api = api;
+    this.data = data;
+    this.config = config;
+  }
+
+  set data(data) {
+    console.log(data);
+    this._data = data;
+  }
+
+  get data() {
+    console.log(this._data);
+    return this._data;
+  }
+
+  createLayout() {
+    this.view.wrapper = CreateDOMElement({
+      tagName: 'div'
+    });
+
+    let headerContent = '';
+    let textContent = '';
+    if (this._data) {
+      headerContent = this._data.header;
+      textContent = this._data.text;
+    }
+
+    this.view.header = new Header({
+      data: {
+        text: headerContent,
+        level: 6,
+      },
+      api: this.api,
+      config: {
+        placeholder: 'Enter title',
+        level: 6
+      }
+    });
+
+    this.view.text = new Paragraph({
+      data: {
+        text: textContent,
+      },
+      api: this.api,
+      config: {
+        placeholder: 'Enter text'
+      }
+    });
+
+    this.view.wrapper.appendChild(this.view.header._element);
+    this.view.wrapper.appendChild(this.view.text._element);
+    return this.view.wrapper;
+  }
+
+  render() {
+    return this.createLayout();
+  }
+
+  save() {
+    return {
+      header: this.view.header._element.innerHTML,
+      text: this.view.text._element.innerHTML,
+    }
+  }
+
+}
