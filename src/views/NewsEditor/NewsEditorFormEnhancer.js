66ad1464e791dafd05666c129b70aef6bd74ffe3
fmakareev <fmakareev@code-artel.com>
2019-09-08 22:14:47 +0300

    add errorHandler, onDelete

--- src/views/NewsEditor/NewsEditorFormEnhancer.js
+++ src/views/NewsEditor/NewsEditorFormEnhancer.js
@@ -4,6 +4,7 @@ import {APOLLO_TYPES} from "../../apollo/Types";
 import {graphql} from '@apollo/react-hoc';
 import {compose} from "recompose";
 import {UpdateArticleMutation} from "./UpdateArticleMutation";
+import {DeleteArticleMutation} from "./DeleteArticleMutation";
 import {CreateArticleMutation} from "./CreateArticleMutation";
 
 import {FORM_ERROR} from "final-form";
@@ -50,6 +51,9 @@ const QueryCompose = compose(
   graphql(UpdateArticleMutation, {
     name: 'UpdateArticles'
   }),
+  graphql(DeleteArticleMutation, {
+    name: 'DeleteArticles'
+  }),
 )
 
 
@@ -60,10 +64,17 @@ const NewsEditorFormEnhancer = (WrapperComponent) =>
       EditorRefInstance: null,
     };
 
+    /**
+     *
+     * @returns
+     * @desc
+     * */
     transformData = (values) => {
       const newValues = Object.assign({}, values);
       if (typeof newValues.head_image !== 'string') {
         newValues.head_image = newValues.head_image && newValues.head_image.file;
+      } else {
+        delete newValues.head_image;
       }
 
       if (typeof newValues.og_thumbnail !== 'string') {
@@ -84,18 +95,39 @@ const NewsEditorFormEnhancer = (WrapperComponent) =>
       if (newValues.is_featured === null) {
         newValues.is_featured = false;
       }
-      if (newValues.with_video === null) {
-        newValues.with_video = false;
+      if (newValues.slug !== null) {
+        newValues.slug = newValues.slug.trim()
+          .replace(/[@:%._\+~#=,]/g,'')
+          .replace(/ /g, '-');
       }
       return newValues;
     };
 
+    errorHandler = (result, callback) => {
+      const errors = {};
+      if (result.networkError) {
+        Object.entries(result.networkError.result).map(([key, value]) => {
+          errors[key] = value[0];
+        });
+        console.log(errors);
+        return {
+          [FORM_ERROR]: result.message,
+          ...errors,
+        }
+      } else if (callback) {
+        callback();
+      }
+    }
+
     onPublish = async (values) => {
       console.log('onPublish: ', values);
+      const newValues = this.transformData(values)
+
       if (values.id) {
         const result = await this.props.UpdateArticles({
           variables: {
-            ...this.transformData(values),
+            ...newValues,
+            method: 'PUT'
           }
         })
           .catch((error) => {
@@ -103,42 +135,23 @@ const NewsEditorFormEnhancer = (WrapperComponent) =>
 
             return JSON.parse(JSON.stringify(error));
           });
-        const errors = {};
-        if (result.networkError) {
-          Object.entries(result.networkError.result).map(([key, value]) => {
-            errors[key] = value[0];
-          });
-          console.log(errors);
-          return {
-            [FORM_ERROR]: result.message,
-            ...errors,
-          }
-        }
+
+        return this.errorHandler(result);
 
       } else {
         const result = await this.props.CreateArticles({
           variables: {
-            ...this.transformData(values),
+            ...newValues,
+            method: 'POST',
           }
         }).catch((error) => {
           console.log(error);
           return JSON.parse(JSON.stringify(error));
         });
         console.log('onPublish create: ', result);
-        const errors = {};
-        if (result.networkError) {
-          Object.entries(result.networkError.result).map(([key, value]) => {
-            errors[key] = value[0];
-          });
+        return this.errorHandler(result, () =>
+          this.props.history.push(`/news-editor/${newValues.slug}`));
 
-          console.log(errors);
-          return {
-            [FORM_ERROR]: result.message,
-            ...errors,
-          }
-        }
-
-        this.props.history.push(`/news-editor/${values.slug}`)
       }
 
     };
@@ -148,6 +161,23 @@ const NewsEditorFormEnhancer = (WrapperComponent) =>
       this.transformData(values);
     };
 
+    onDelete = async (values) => {
+      //
+      const result = await this.props.DeleteArticles({
+        variables: {
+          slug: values.slug.trim(),
+        }
+      })
+        .catch((error) => {
+          console.log(error);
+          return JSON.parse(JSON.stringify(error));
+        });
+      console.log(result);
+      this.props.history.push(`/news`)
+      // return this.errorHandler(result, () =>
+      //   this.props.history.push(`/news`));
+    }
+
     render() {
       const {data, match} = this.props;
 
@@ -155,6 +185,7 @@ const NewsEditorFormEnhancer = (WrapperComponent) =>
         {...this.props}
         onPublish={this.onPublish}
         onDraft={this.onDraft}
+        onDelete={this.onDelete}
         loading={data ? data.loading : !!(match.params && match.params.slug)}
         EditorRefInstance={{
           EditorRefInstance: this.state.EditorRefInstance,
@@ -175,3 +206,26 @@ const NewsEditorFormEnhancer = (WrapperComponent) =>
   });
 
 export default NewsEditorFormEnhancer;
+
+
+
+
+
+
+
+
+const t = {
+
+  filter: [
+    {
+      name:'Select seasons',
+      type: 'text',
+    },
+    {
+      name:'Weight category',
+      type: 'text',
+      dependOn: 'Select seasons'
+    },
+  ]
+
+}
